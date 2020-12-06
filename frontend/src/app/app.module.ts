fc4eaded651be8077d7081442ad9913446f6e9d5
kalisi97 <katarinasimic1997@outlook.com>
2020-12-06 23:28:28 +0100

    Added service and model classes to Angular project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomerComponent } from './customers/customer/customer.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemComponent } from './items/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderComponent,
+    OrderItemsComponent,
+    CustomersComponent,
+    CustomerComponent,
+    ItemsComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
