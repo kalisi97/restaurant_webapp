8d82a134f67605befc9b858898efe1bf93e0036a
kalisi97 <katarinasimic1997@outlook.com>
2020-12-14 23:48:42 +0100

    Order items form modified

--- frontend/src/app/orders/order-items/order-items.component.ts
+++ frontend/src/app/orders/order-items/order-items.component.ts
@@ -1,45 +1,44 @@
-import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { Inject } from '@angular/core';
 import { OrderItem } from 'src/app/shared/order-item.model';
 import { ItemService } from 'src/app/shared/item.service';
-import { Item } from 'src/app/shared/item.model';
-import { OrderService } from 'src/app/shared/customerorder.service';
+import {Item} from 'src/app/shared/item.model';
 import { NgForm } from '@angular/forms';
+import { OrderService } from 'src/app/shared/customerorder.service';
 
 @Component({
   selector: 'app-order-items',
   templateUrl: './order-items.component.html',
   styleUrls: []
 })
-
 export class OrderItemsComponent implements OnInit {
-  
-  formData: OrderItem;
-  itemList: Item[];
-  isValid: boolean = true;
-
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data,
-    public dialogRef: MatDialogRef<OrderItemsComponent>,
-    public ItemService: ItemService,
-    public orderService: OrderService) { }
+formData: OrderItem;
+itemList: Item[];
+isValid: boolean = true;
+constructor(
+  @Inject(MAT_DIALOG_DATA) public data,
+  public dialogRef: MatDialogRef<OrderItemsComponent>,
+  public ItemService: ItemService,
+  public orderService: OrderService
+ ) { }
 
   ngOnInit(): void {
-    this.ItemService.getItemsList().then(res => this.itemList = res as Item[]);
 
-        this.formData = {
-          OrderItemId: null,
-          OrderId: this.data.OrderId,
-          ItemId: 0,
-          ItemName: '',
-          Price: 0,
-          Quantity: 0,
-          Total: 0
-        } 
-
+this.ItemService.getItemsList().then(res => this.itemList = res as Item[]);
+if (this.data.orderItemIndex == null)
+    this.formData = {
+      OrderItemId: null,
+      OrderId: this.data.OrderId,
+      ItemId: 0,
+      ItemName: '',
+      Price: 0,
+      Quantity: 0,
+      Total: 0
+    } 
+    else
+    this.formData = Object.assign({}, this.orderService.orderItems[this.data.orderItemIndex]);
   }
-
   updatePrice(ctrl) {
     if (ctrl.selectedIndex == 0) {
       this.formData.Price = 0;
@@ -66,7 +65,6 @@ export class OrderItemsComponent implements OnInit {
     }
      }
 
-
   validateForm(formData: OrderItem) {
     this.isValid = true;
     if (formData.ItemId == 0)
@@ -75,6 +73,5 @@ export class OrderItemsComponent implements OnInit {
       this.isValid = false;
     return this.isValid;
   }
-
 
 }
